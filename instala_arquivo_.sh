b589c6e69955344b8134a9727d65cade7ab82100
O Fantástico Senhor Chimposo <65960679+devchimpa@users.noreply.github.com>
2023-08-29 12:15:17 -0300

    Create instala_arquivo_.sh

---
+++ instala_arquivo_.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# script para ser utilizado para instalar os arquivos necessários para o script de virus.
+
+echo " Iniciando a instalação..."
+
+sleep 2
+
+apt-get install clamav clamav-docs clamav-daemon clamav-freshclam
+
+sleep 2
+
+echo " Anti virus instalado. Instalando arquivos complementares..."
+
+sleep 2
+
+apt-get install arc arj bzip2 cabextract lhasa lzop nomarch p7zip-full pax rpm tnef unrar-free unzip zip
+
+sleep 2
+
+echo " Inicializando os daemons ..."
+
+sleep 1
+
+systemctl restart clamav-freshclam
+
+sleep 1
+
+systemctl restart clamav-daemon
+
+echo " Instalação concluída. "
