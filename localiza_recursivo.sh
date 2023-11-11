3f9d9bcfb811051ac7c22a6c876e794ae117dab8
O Fantástico Senhor Chimposo <65960679+devchimpa@users.noreply.github.com>
2023-11-10 21:07:24 -0300

    Update localiza_recursivo.sh

--- localiza_recursivo.sh
+++ localiza_recursivo.sh
@@ -86,3 +86,136 @@ done
   }
 
 procura $DIRETORIO_DEFINIDO
+root@cmxSRVBKP01:/home# vi localiza_recursivo.sh 
+root@cmxSRVBKP01:/home# 
+root@cmxSRVBKP01:/home# cat localiza_recursivo.sh 
+#!/bin/bash
+#
+#
+#
+#
+# Feito por: Rodrigo Pinheiro, 10 de Novembro de 2023.
+#
+# --------------------------------------------------
+# Modificado por:( caso mexa no script, coloque nome e data.)
+# -------------------------------------------------
+# Modificado por:
+#
+#
+#
+# Este script não tem dono, sinta-se à vontade para melhorá-lo...
+#
+#
+#
+#
+##########################################################################################
+#
+#
+# O objetivo deste script é buscar gravações dentro do diretório definido na função procura()
+#
+# O script funciona por meio de um laço for que vai fazendo a comparação para identificar
+# se os arquivos localizados na varredura são os mesmos que solicitamos, caso seja
+# ele irá registrar um log indicando a localização do arquivo.			
+#
+#-----------------------------------------------------------------------------------------
+################################ Boas práticas ############################################
+#------------------------------------------------------------------------------------------
+#
+# O script está todo comentado e comentários são sempre bem vindos.
+# Caso acrescente algo, comente, coloque data e assinatura para
+# que todos saibam o que foi alterado, quando e por quem para caso de dúvidas.
+# 
+# Mantenha variáveis GLOBAIS em maiúsculas
+#
+# Mantenha nomes minimamente claros e em português
+# 
+# 
+# 
+#
+############################################################################################
+#
+#			 CONFIGURAÇÕES DO SCRIPT:
+#		( utilize este campo para configurar o script)
+#
+#########################################################################################
+#
+#
+#diretorio onde o script deve iniciar a busca:
+DIRETORIO_DEFINIDO=/mnt2
+#
+#
+LISTA_DE_BUSCAS=/home/lista_de_procuradas
+#
+#arquivo onde devem ser registradas gravacoes localizadas:
+ARQUIVO_DE_LOG=/home/gravacao_localizada
+#
+
+
+
+############################## INICIO DO SCRIPT #############################################
+
+
+
+# Esta função servirá para comparar se o arquivo que ele achou é o mesmo que estamos procurando.
+
+compara(){
+
+  LOCALIZADO=$( echo $1 | tr "/" " " | rev | awk '{ print $1 }' | rev )
+
+# aqui para cada linha do arquivo ele fará a comparação com o arquivo localizado.
+for linha in $(cat $LISTA_DE_BUSCAS)
+        do
+        GRAVACAO=$linha
+# se a gravacao que procuramos for igual a gravacao que ele achou, então ele ira registrar no log
+  if [ "$GRAVACAO" = "$LOCALIZADO" ]
+
+  then
+	echo "$1 achou!"
+       	echo $1 >> $ARQUIVO_DE_LOG
+
+	fi
+
+done
+}
+
+
+
+
+# Esta função serve para varrer os diretórios que definimos para a busca.
+# Ele irá começar a procura a partir dali e irá procurar recursivamente em todos os diretórios.
+
+procura(){
+
+# A Função recebe como parâmetro o diretorio
+  DIRETORIO_A_VARRER=$1
+
+  for arquivo in "$DIRETORIO_A_VARRER"/*
+do
+# Se for um arquivo ele verifica se este é igual o que estamos procurando
+  if [ -f "$arquivo" ]
+  then
+
+  compara $arquivo
+
+# se for um diretório, ele entra no diretório e continua a varredura.
+
+elif [ -d "$arquivo" ]
+then
+    procura $arquivo
+  fi
+
+done
+
+  }
+
+
+######################## Para ativar/desativar o script #########################################
+############################ comente ou descomente.##############################################
+#################################################################################################
+# aqui inicia o script, neste trecho estamos chamando o script para começar a varredura
+# no diretório configurado lá em cima.
+
+procura $DIRETORIO_DEFINIDO
+
+
+##############################################################################################
