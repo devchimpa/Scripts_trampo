a9bde7cae1e9418805c5bdad3fe0b858fca46857
O Fantástico Senhor Chimposo <65960679+devchimpa@users.noreply.github.com>
2024-03-06 14:13:20 -0300

    Update mata_cmx.sh

--- mata_cmx.sh
+++ mata_cmx.sh
@@ -9,10 +9,19 @@
 #
 #########################################################################
 #
-#       OBJETIVO:
+#
+#       DESCRIÇÃO:
 #       Este script deve fazer um stop gracefully no comunix a 00h
 #       dos sábados e quando não houver mais chamadas em curso
 #       o serviço deve ser encerrado e inicializado novamente.
+#
+#       !! IMPORTANTE !!
+#
+#       O script deve ser configurado no crontab da seguinte maneira:
+#       0 0 * * 6 /home/extend/scripts/stop-fully.sh
+#
+#       E o script deverá estar em:
+#       /home/extend/scripts/stop-fully.sh
 #
 #
 ############## Siga o modelo abaixo caso mexa no script: ################
@@ -28,18 +37,20 @@
 #                       VARIAVEIS DE CONFIGURAÇÃO:                       #
 ##########################################################################
 
+
+
 # tempo de espera em SEGUNDOS para aguardar as chamadas encerrarem.
-TEMPO_DE_ESPERA=60
+TEMPO_DE_ESPERA=10
+
+# Esta é uma variável de controle, ela que determina as ações do script e não deve ser alterada.
+CHAMADAS_ATIVAS=$( comunix -rx "core show calls" | grep "active" | awk {'print $1'} 2>>/dev/null )
 
 # data para registros de logs
 DATA_DE_INICIO=$(  date +%d-%m-%y_%H:%M )
 
-# Verificador de chamadas em andamento.
-#CHAMADAS_ATIVAS=$( comunix -rx "core show calls" | grep "active" | awk {'print $1'} 2>>/dev/null  )
-
-echo $CHAMADAS_ATIVAS
-
 LOG_DE_ERRO="/home/extend/erro-$0"
+
+
 
 ##########################################################################
 #                         FUNCOES DO SCRIPT:                             #
@@ -48,13 +59,16 @@ LOG_DE_ERRO="/home/extend/erro-$0"
 
 
 valida_chamadas(){
-# Esta função guarda quantas chamadas estão em andamento.
+# Esta função guarda quantas chamadas estão em andamento
+# e altera para zero caso o valor seja vazio.
+# o valor sendo zero, o Comunix será reiniciado por outras funções.
 
         if [ -z "$CHAMADAS_ATIVAS" ]
         then
-        CHAMADAS_ATIVAS=0
+        VALIDADOR=0
         else
         CHAMADAS_ATIVAS=$( comunix -rx "core show calls" | grep "active" | awk {'print $1'} 2>>/dev/null )
+        VALIDADOR=1
         fi
 }
 
@@ -64,17 +78,16 @@ stop_gracefully(){
 
         echo "Stop Grecefully!"
         comunix -rx "core stop gracefully"
-        # Este trecho irá verificar se as chamadas ainda estão em andamento.
-        # Este valor deve ser zero.
 
-        while [ "$CHAMADAS_ATIVAS" -ne 0 ]
+
+        while [ "$VALIDADOR" -ne 0 ]
 
         do
                 # aguarda um período para encerrar o Comunix.
                 sleep "$TEMPO_DE_ESPERA"
                 echo "Aguardando encerramento..."
 
-        # chama função para determinar se as chamadas zeraram.
+                # chama a função para determinar se as chamadas zeraram.
                 valida_chamadas
 done
 
@@ -86,7 +99,7 @@ done
 encerra_comunix(){
 
     #esta função irá encerrar o Comunix caso a quantidade de chamadas em curso seja igual a zero
-    if [ "$CHAMADAS_ATIVAS" -eq 0 ]
+    if [ "$VALIDADOR" -eq 0 ]
 then
 
         pkill comunix
