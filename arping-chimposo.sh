81f0cba649b0aa4e56508d3bd32db60124c56103
O Fantástico Senhor Chimposo <65960679+devchimpa@users.noreply.github.com>
2024-06-04 11:19:40 -0300

    Create arping-chimposo.sh

---
+++ arping-chimposo.sh
@@ -0,0 +1,51 @@
+# Desenvolvido por: DevChimpa
+# Data de Criação: 29/05/2024
+
+# Contato:
+# https://www.linkedin.com/in/rodrigo-pinheiro-214663206/
+# https://github.com/devchimpa/
+#
+#######################################################################
+#
+#######################-DESCRIÇÃO DO PROGRAMA-###########################
+
+# Este script verifica se o servidor Share está com o IP virtual
+# caso esteja, ele irá rodar o arping, do contrário ele encerra.
+# este script roda nos dois servidores Share.
+
+########################################################################
+#
+##############-Siga o modelo abaixo caso mexa no script:-###############
+#
+# Modificado por:
+# Data:
+# Contato:
+# Modificação feita:
+#
+#########################################################################
+
+
+
+roda_arping(){
+
+
+      arping -i eth0 -c2  -S 192.168.1.63 -B
+      arping -i eth0 -c2 -S 192.168.1.63 192.168.0.10
+
+}
+
+
+IP_VIRTUAL="$( ifconfig | grep 192.168.1.63 | tr ":" " " | awk {'print $3'} )"
+
+        if [ -x $IP_VIRTUAL ]
+
+        then
+
+        echo "sem ip"
+
+
+        else
+
+        roda_arping
+
+fi
