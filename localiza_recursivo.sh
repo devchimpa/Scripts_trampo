45d8c057fc5b9e8325bc4b06eb6ae83ecb897cae
O Fantástico Senhor Chimposo <65960679+devchimpa@users.noreply.github.com>
2024-07-08 15:12:05 -0300

    Update localiza_recursivo.sh

--- localiza_recursivo.sh
+++ localiza_recursivo.sh
@@ -1,153 +1,85 @@
 #!/bin/bash
+
+#Desenvolvido por: Rodrigo Pinheiro
+#Data inicial: 05/03/2024
+
+#Contato:
+#https://www.linkedin.com/in/rodrigo-pinheiro-214663206/
+#https://github.com/devchimpa/
 #
 #
 #
+# COMO FUNCIONA:
+# Para executar o script devem ser montados os volumes contendo as
+# gravações com o nome: /mnt.
+# Os uniqueids devem ser colocados dentro de: /home/lista_de_procuradas.
+# Após isso, basta rodar o script.
 #
-# Feito por: Rodrigo Pinheiro, 10 de Novembro de 2023.
-#
-# --------------------------------------------------
-# Modificado por:( caso mexa no script, coloque nome e data.)
-# -------------------------------------------------
-# Modificado por:
-#
-#
-#
-# Este script não tem dono, sinta-se à vontade para melhorá-lo...
+# Este é um programa colaborativo, caso tenha melhorias a acrescentar, sinta-se
+# a vontade para mexer, apenas siga o modelo abaixo informando o que foi feito.
 #
 #
+############## Siga o modelo abaixo caso mexa no script: ###############
 #
 #
-##########################################################################################
+#Modificado por:
+#Data:
+#Contato:
+#Modificação feita:
 #
 #
-# O objetivo deste script é buscar gravações dentro do diretório definido na função procura()
-#
-# O script funciona por meio de um laço for que vai fazendo a comparação para identificar
-# se os arquivos localizados na varredura são os mesmos que solicitamos, caso seja
-# ele irá registrar um log indicando a localização do arquivo.
-#
-#-----------------------------------------------------------------------------------------
-################################ Boas práticas ############################################
-#------------------------------------------------------------------------------------------
-#
-# O script está todo comentado e comentários são sempre bem vindos.
-# Caso acrescente algo, comente, coloque data e assinatura para
-# que todos saibam o que foi alterado, quando e por quem para caso de dúvidas.
-#
-# Mantenha variáveis GLOBAIS em maiúsculas
-#
-# Mantenha nomes minimamente claros e em português
-#
-#
-#
-#
-############################################################################################
-#
-#                        CONFIGURAÇÕES DO SCRIPT:
-#               ( utilize este campo para configurar o script)
-#
-#########################################################################################
-#
-#
-#diretorio onde o script deve iniciar a busca:
-DIRETORIO_DEFINIDO=/mnt2
-#
-#Lista que será usada pelo script para leitura, os arquivos que queremos localizar
-#devem estar dentro dela
-LISTA_DE_BUSCAS=/home/lista_de_procuradas
-#
-#arquivo onde devem ser registradas gravacoes localizadas:
-ARQUIVO_DE_LOG=/home/gravacao_localizada
-#
+##########################################################################
+#                       VARIAVEIS PRINCIPAIS:                            #
+##########################################################################
+
+# aqui deve se colocado o nome do ponto de montagem, no caso mnt
+
+NOME_DIRETORIO="mnt"
+
+
+DIRETORIOS=$( df -h | grep $NOME_DIRETORIO | awk '{print $6}' )
 
 
 
-############################## INICIO DO SCRIPT #############################################
+##########################################################################
+#                           CORPO DO SCRIPT:                             #
+##########################################################################
+# Esta função serve para alterar o arquivo de listas
+edita_arquivo()
+        {
 
-
-# Esta funcao serve para leitura do arquivo de gravacoes solicitadas
-# caso a lista esteja zerada o script encerra.
-
-verifica_procura(){
-
-        arquivo_vazio=$( wc -w $LISTA_DE_BUSCAS | tr -d "a-z" | tr -d "_"  | tr -d "/" )
-
-        if [ $arquivo_vazio -eq 0 ]
-        then
-        echo "Lista Finalizada."
-        exit 0
-fi
+        echo " Editando a lista de gravações "
+        sed -i "s/$i//g" /home/lista_de_procuradas
+        sleep 1
+        echo " Lista de gravações editada. "
+        sleep 1
 }
 
+#Esta função irá fazer a busca dos arquivos da lista
+procura_lista()
+        {
+         for i in $( cat /home/lista_de_procuradas )
+         do
+         printf "Verificando arquivo: %s\r" "$i"
+         PROCURADA=$( find $1 -iname $i )
 
-# Esta função servirá para comparar se o arquivo que ele achou é o mesmo que estamos procurando.
+         if [ -z "$PROCURADA" ]
+         then
+                 echo "$i Não localizada."
+         else
+                for GRAVA in ${PROCURADA[*]}
+                do
+         cp -rpv "$GRAVA" /home/gravacoes/localizadas
+         echo "$GRAVA localizada" >> /home/gravacoes/localizadas/lista-encontradas
+         sleep 1
+         edita_arquivo
+                done
+                fi
 
-compara(){
-
-  LOCALIZADO=$( echo $1 | tr "/" " " | rev | awk '{ print $1 }' | rev )
-
-# aqui para cada linha do arquivo ele fará a comparação com o arquivo localizado.
-for linha in $(cat $LISTA_DE_BUSCAS)
-        do
-        GRAVACAO=$linha
-# se a gravacao que procuramos for igual a gravacao que ele achou, então ele ira registrar no log
-  if [ "$GRAVACAO" = "$LOCALIZADO" ]
-
-  then
-        echo
-        echo "$1 achou!"
-        #estes echo vazios servem para criar um espaço entre as gravaçoes localizadas e deixar a visibilidade melhor
-        echo
-        echo $1 >> $ARQUIVO_DE_LOG
-
-        fi
-
-done
-}
-
-
-
-
-# Esta função serve para varrer os diretórios que definimos para a busca.
-# Ele irá começar a procura a partir dali e irá procurar recursivamente em todos os diretórios.
-
-procura(){
-
-# A Função recebe como parâmetro o diretorio
-  DIRETORIO_A_VARRER=$1
-
-  for arquivo in "$DIRETORIO_A_VARRER"/*
+        done
+        }
+# Neste ponto é utilizado o find em cada ponto de montagem na lista
+for PONTO_DE_MONTAGEM in ${DIRETORIOS[*]}
 do
-
-  verifica_procura
-
-# Se for um arquivo ele verifica se este é igual o que estamos procurando
-  if [ -f "$arquivo" ]
-  then
-# este printf serve para que seja possivel visualizar a verificação
-# caso nao queira visualizar, basta comentar.
- printf "Verificando arquivo: %s\r" "$arquivo"
-  compara $arquivo
-# se for um diretório, ele entra no diretório e continua a varredura.
-
-elif [ -d "$arquivo" ]
-then
-    procura $arquivo
-  fi
-
+        procura_lista "$PONTO_DE_MONTAGEM"
 done
-
-}
-
-
-######################## Para ativar/desativar o script #########################################
-############################ comente ou descomente.##############################################
-#################################################################################################
-# aqui inicia o script, neste trecho estamos chamando o script para começar a varredura
-# no diretório configurado lá em cima.
-
-procura $DIRETORIO_DEFINIDO
-echo
-echo " Programa finalizado..."
-
-##############################################################################################
