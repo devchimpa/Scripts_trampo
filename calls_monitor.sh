72649d2fee84e8bcba7b6c8907c87f6df8316594
O Fantástico Senhor Chimposo <65960679+devchimpa@users.noreply.github.com>
2024-08-26 17:22:16 -0300

    Update calls_monitor.sh

--- calls_monitor.sh
+++ calls_monitor.sh
@@ -14,79 +14,85 @@
 #                                                                          #
 #       DESCRIÇÃO:                                                         #
 #                                                                          #
-#       O objetivo do programa é acompanhar a quantidade de bilhetes
-#       na main e caso haja um volume alto, reiniciar o processo...        #
-#                                                                          #
+#       O objetivo do programa é acompanhar a quantidade de bilhetes       #
+#       no diretorio /home/extend/calls/main                               #
+#       e caso haja um volume alto, reiniciar o processo...                #
+#       !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!         #
+#       Este script necessita do pacote sudo para executar a função        #
+#               de reiniciar o trata                                               #
 #                                                                          #
 ############################################################################
 
-
-
 # VARIÁVEIS DE CONFIGURAÇÃO
-
 # Diretório base onde as pastas dos dias estão localizadas
 DIRETORIO="/home/extend/calls/main"
 
-# AQUI DEVE SER INSERIDO O VALOR CONSIDERADO ALTO
-# PARA A QUANTIDADE DE BILHETES
+# ESTA VARIAVEL CAPTURA O ANO ATUAL
+# PARA CONTABILIZAR OS BILHETES DO ANO
+ANO=$( date +%Y )
 
-VOLUME=50
+# AQUI DEVE SER INSERIDO O VALOR CONSIDERADO ALTO
+# PARA A QUANTIDADE DE BILHETES NO SERVIDOR
+LIMITE=50
+
+# ESTA VARIAVEL É A SOMA DOS BILHETES
+# E DEVE SER CONFIGURADA COM O VALOR 0
+# ELA SERA COMPARADA COM A VARIAVEL LIMTE.
+SOMA="0"
+
+########################################################################
+
+#VALORES INTERPRETADOS PELO ZABBIX
+
+ERROR="1"
+
+OK="0"
 
 ######################## FUNÇÕES #######################################
 
-
 mata_e_sobe_trata(){
 
+PID_DO_TRATA=$( ps aux | grep  /home/extend/./trata_send_bilhete_tcp | grep -v grep | awk {'print $2'} )
 
-PID_DO_TRATA=$( ps aux | grep  /home/extend/./trata_estatistica | grep -v grep | awk {'print $2'} )
-kill -9 $PID_DO_TRATA
-/home/extend/trata_estatistica &
+# este trecho precisa de sudo para ser executado pelo usuario zabbix
+        sudo kill -9 $PID_DO_TRATA
+
+        sudo /home/extend/./trata_send_bilhete_tcp &
 
 }
 
 # FUNÇÃO PARA VERIFICAR OS BILHETES
 
-check_bilhetes() {
-for diretorio_dia in "$DIRETORIO"/2024-*/
+contagem_bilhetes() {
+# esta funçao faz a contabilidade
+# de bilhetes por dia
+
+for diretorio_dia in "$DIRETORIO"/"$ANO"-*/
 do
         if [ -d "$diretorio_dia" ]
          then
                 CONTAGEM=$(ls "$diretorio_dia" | wc -l)
-
-                if [ "$CONTAGEM" -gt "$VOLUME" ]
-         then
-       echo "ALERTA: O diretorio $diretorio_dia contem: $CONTAGEM bilhetes."
-       # Neste ponto será reiniciado processo do trata para reprocessar os bilhetes novamente.
-           mata_e_sobe_trata
-      fi
+                                SOMA=$( expr "$CONTAGEM" + "$SOMA" )
     fi
   done
 }
 
+valida_limite(){
 
-# FUNÇÃO INFINITA PARA SEMPRE VERIFICAR OS BILHETES
-roda_script(){
+        if [ "$SOMA" -gt "$LIMITE" ]
+        then
+                echo "$ERROR"
+                # caso não tenha o pacote sudo, deixar
+                # a funcao abaixo comentada
+                #mata_e_sobe_trata
+        else
+                echo "$OK"
+        fi
 
-while true
-
- do
-
-  check_bilhetes
-
-  sleep 1
-
-done
 }
 
 ##############################################################################################
                                #CHAMADA DE FUNÇÕES#
 ##############################################################################################
-
-# O SCRIPT PRINCIPAL É O roda_script ELE QUEM DEVE SER DESCOMENTADO.
-# AS OUTRAS FUNÇÕES DEVEM SER HABILITADAS APENAS PARA FINS DE TESTES.
-
-#mata_e_sobe_trata
-
-#check_bilhetes
-
-roda_script
+contagem_bilhetes
+valida_limite
