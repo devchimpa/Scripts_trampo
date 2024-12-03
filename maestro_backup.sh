bb88b5c1a99b5874d324d9e0fa38e8e2c6158b59
O Fantástico Senhor Chimposo <65960679+devchimpa@users.noreply.github.com>
2024-12-03 10:13:57 -0300

    Update maestro_backup.sh

--- maestro_backup.sh
+++ maestro_backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#
+---		
 # Desenvolvido por: DevChimpa
 # Data de Criação: 22/11/2024
 
@@ -11,12 +11,12 @@
 #
 #######################-DESCRIÇÃO DO PROGRAMA-###########################
 #
-#       O objetivo deste script é funcionar como um maestro que
-#       orquestra os backups feitos pelo Ansible.
-#       Ele serve para que os backups sigam uma ordem para nao
-#       sobrecarregar a rede durante as copias.
-#       Este script pode ser melhorado, sinta-se a vontade para
-#       modificar e melhorar as funcionalidades dele e dos playbooks.
+#	O objetivo deste script é funcionar como um maestro que
+#	orquestra os backups feitos pelo Ansible.
+#	Ele serve para que os backups sigam uma ordem para nao
+#	sobrecarregar a rede durante as copias.
+#	Este script pode ser melhorado, sinta-se a vontade para
+#	modificar e melhorar as funcionalidades dele e dos playbooks.
 #
 ########################################################################
 #
@@ -37,59 +37,64 @@
 
 destino_backup="/home/bkp_ansi/"
 
-lista_de_clientes="/home/extend/scripts/maestro_bkp/lista_de_clientes"
+
+controle_de_clientes="/home/extend/scripts/maestro_bkp/controle"
+
 
 CODIGO="/home/extend/scripts/maestro_bkp/codigo_cliente"
 
+
 CODIGO_CLIENTE=$( cat /home/extend/scripts/maestro_bkp/codigo_cliente )
+
 
 HORARIO_INICIO=00
 
+
 HORARIO_FINAL=23
 
-#########################################################################
 
-
-#target_host=$( cat "$lista_de_clientes" | grep "$CODIGO_CLIENTE" | awk -F ":" {'print $2'} )
-
-
-valida_horario(){
-
-# esta funcao define o horario que inicia e termina o script
-
-        if [ $(date +%H ) -lt "$HORARIO_INICIO" ]
-
-        then
-                echo "Fora de horário."
-                exit 0
-
-        elif [ $(date +%H ) -gt "$HORARIO_FINAL" ]
-
-        then
-
-                echo "Fora de horário."
-        date >> /home/extend/scripts/maestro_bkp/horario_finalizado
-                exit 0
-
-        else
-                # chamada de funcao para verificar em que ponto da fila o backup esta
-                valida_codigo_cliente
-
-                fi
-
-        }
+QUANTIDADE=$( cat "$controle_de_clientes" | wc -l )
 
 
 
+#ansible-playbook -i /etc/ansible/inventories/teste/teste_ura.yml backup_rsync_sshpass.yml \
+#  --extra-vars "target_host="$ALVO" diretorio_destino=/home/bkp_ansi/"$CLIENTE"/ senha="$SENHA"
+
+
+
+CLIENTE_ATUAL=$( cat $controle_de_clientes)
+
+SENHA=$( cat /etc/ansible/inventories/teste/teste_ura.yml  | grep pass | awk  -F ":" {'print $2'} | tr -d " " )
+
+
+###################
+controle_de_fluxo(){
+
+SOLUCAO=$( cat $controle_de_clientes | awk -F ":" {'print $3'} )
+CLIENTE=$( cat $controle_de_clientes | awk -F ":" {'print $2'} )
+CODIGO=$( cat $controle_de_clientes | awk -F ":" {'print $1'} )
+
+ALVO="$CLIENTE"_"$SOLUCAO"
+
+echo "$CODIGO:$CLIENTE:$SOLUCAO" > controle
+
+
+
+#ansible-playbook -i /etc/ansible/inventories/teste/teste_ura.yml \
+#  /etc/ansible/playbooks/backup_rsync_sshpass_parametros.yml \
+#  --extra-vars "target_host=$ALVO diretorio_destino=/home/bkp_ansi/$CLIENTE/$SOLUCAO senha=$SENHA"
+
+
+
+ENDERECOS=$( cat /etc/ansible/inventories/"$CLIENTE"/"$CLIENTE"_"$SOLUCAO".yml | grep [0-9] | grep -v pass | tr -d : | tr -d " " )
+
+}
+
+
+#####################
 valida_codigo_cliente(){
 
-# Este trecho ira pegar na fila o proximo
-# cliente a ser feito o backup.
 
-QUANTIDADE=$( cat "$lista_de_clientes" | wc -l )
-
-# primeiro ele verifica se a lista chegou ao fim
-# caso tenha chegado, ele ira para o primeiro da fila
 if [ "$CODIGO_CLIENTE" -gt "$QUANTIDADE" ]
         then
                 CODIGO_CLIENTE=1
@@ -100,63 +105,16 @@ if [ "$CODIGO_CLIENTE" -gt "$QUANTIDADE" ]
 while [ "$CODIGO_CLIENTE" -le "$QUANTIDADE" ]
 
         do
-        executa_playbook
 
-        sleep 2
+echo "$CLIENTE"
 
 
 done
-
 }
 
 
-executa_playbook(){
-# esta funcao chama o playbook para executar e enviar para o diretorio correto.
 
-# este trecho define o cliente e para onde sera enviado
-
-# esta variavel ira concatenar os nomes para o backup
-cliente=$( cat "$lista_de_clientes" | grep "$CODIGO_CLIENTE" | awk -F ":" {'print $2'} )
+####################################################################################################
+controle_de_fluxo
+#valida_codigo_cliente
 #
-
-#sub_dir=$( echo "$target_host" | awk -F "_" {'print $1'})
-
-for sub_dir in $( cat /home/extend/scripts/maestro_bkp/lista_de_solutions )
-do
-        target_host="$cliente"_"$sub_dir"
-
-/usr/bin/ansible-playbook -i /etc/ansible/inventories/"$sub_dir"/"$target_host".yml \
-  /etc/ansible/playbooks/backup_diario_parametros.yml \
-  --extra-vars "target_host=$target_host destino_backup=$destino_backup/$sub_dir"
-
-
-#/usr/bin/ansible-playbook /etc/ansible/playbooks/backup_diario_parametros.yml \
-#  --extra-vars "target_host=$target_host destino_backup=$destino_backup/$sub_dir"
-
-
-
-
-
-
-        echo "$target_host"
-        sleep 2
-
-done
-
-        PROXIMO_CLIENTE=$( expr "$CODIGO_CLIENTE" + 1 )
-
-        echo "$PROXIMO_CLIENTE" > "$CODIGO"
-
-        CODIGO_CLIENTE=$( cat "$CODIGO" )
-
-
-        valida_horario
-
-}
-
-
-
-
-
-############################ CHAMADA DE FUNCOES ##################################
-valida_horario  #primeira funcao do script
