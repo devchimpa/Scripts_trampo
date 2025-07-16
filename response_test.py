196db427e761eca5a1a077de889e276ccc096a3e
O Fantástico Senhor Chimposo <65960679+devchimpa@users.noreply.github.com>
2025-07-15 23:52:50 -0300

    Update response_test.py

--- response_test.py
+++ response_test.py
@@ -1,60 +1,65 @@
 #!/usr/bin/python3
 import os
 
-######## counters is just a space to keep the values while the
-# script is workin
 
-response_error=0
-response_success=0
-response_critical=0
-response=0
+logs_directory="/home2/backups/caderno/estudos/python/logs/"
 
-########
-# here is the directory where the logs are
-logs_directory="/home2/backups/caderno/estudos/python/logs"
+def read_logs():
+    
+    total_response= 0
+    total_response= 0
+    total_error= 0
+    total_success= 0
+    total_critical= 0
 
-# here the script is reading the logs
-for archive in os.listdir(logs_directory):
-    path_logs= os.path.join(logs_directory, archive)
-#    print(path_logs)
-# the errors and successful requests are being counted
-    with open (path_logs, 'r') as log_lines:
-        for linha in log_lines:
+    print('#'*31, "RESULTS", '#'*30)  
+    
+    log_results={}
+
+    for archive in os.listdir(logs_directory):
+        path_logs = logs_directory + archive
+   
+        with open(path_logs, 'r') as content_log:
+            results_logs=content_log.read()
+            log_name=archive[4:14:]
+            response_log=results_logs.count("RESPONSE")
+            response_error=results_logs.count("ERROR")
+            response_success=results_logs.count("Success")
+            response_critical=results_logs.count("CRITICAL")  
+            #print(response_critical)
+            print(f'{log_name}: Responses: {response_log} Successful: {response_success} Errors: {response_error} Critical: {response_critical} ' )
                 
-                if "error" in linha.lower():
-                    response_error  += 1
-                elif "critical" in linha.lower():
-                     response_critical += 1
-                elif "success" in linha.lower():
-                     response_success += 1
+            total_response += response_log
+            total_error += response_error
+            total_success += response_success
+            total_critical += response_critical
+
+            log_results[log_name]={
+                'responses':response_log,
+                'errors':response_error,
+                'success':response_success,
+                'critical':response_critical
+                }
             
-response= response_error + response_critical + response_success
-# Now, the script is processing the information collected from logs
-# and is deciding which message will be shown
-percent_success = ( response_success / response ) * 100
-percent_critical = ( response_critical / response ) * 100
-percent_error = ( response_error / response ) * 100
+    print('#'*70)        
+    percent_success=( total_success / total_response * 100 )
+    percent_error=( total_error / total_response * 100 )
+    percent_critical=( total_critical / total_response * 100 )
+    print(f'Succesful: {percent_success:.2f}% Errors: {percent_error:.2f}% Critical: {percent_critical:.2f}%')
+    print('#'*70)  
 
-print("#"*50)
-print( f'Total of requests: {response}')
-print( f'Total of successful: {response_success}')
-print( f'Total of errors: {response_error} ')
-print( f'Total of critical: {response_critical}')
-print("#"*50)
-###############################################
-print("Values of percents:")
-print( f'Sucessful = {percent_success:.2f}%' )
-print( f'Errors = {percent_error:.2f}%' )
-print( f'Critical = {percent_critical:.2f}%' )
-print("#"*50)
+    for item in log_results.keys():
+        #time.sleep(1)
+        response_item=(log_results[item]['responses'])
+        error_item=(( log_results[item]['errors']) / response_item * 100)
+        critical_item=((log_results[item]['critical']) / response_item * 100)
+        if error_item > 30 and critical_item > 10:
+            print(f'The {item.capitalize()} is not operating properly and must be checked immediately.')
+        elif error_item > 30 and critical_item < 10:
+            print(f'The {item.capitalize()} needs the attention of the IT administrator.')
+        else:
+            print(f'The {item.capitalize()} is operating properly.')
+        
 
-if int(percent_error) > 30 and int(percent_critical) > 10:
-     print("The equipment is not operating properly and must be checked immediately")
-elif int(percent_error) > 30 and int(percent_critical) < 10:
-     print("The equipment needs the attention of the IT administrator.")
-elif int(percent_error) < 30 and int(percent_critical) > 10:
-     print("The equipment needs the attention of the IT administrator.")
-else:
-     print("The equipment is operating properly.")
+read_logs()
 
-print("#"*50)
